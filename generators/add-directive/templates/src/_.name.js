a08bbab99fae4136ebc53053f482c1357d7b68a7
danielxiaowxx <xiaohutu136@163.com>
2016-03-15 20:40:46 +0800

    update

--- generators/add-directive/templates/src/_.name.js
+++ generators/add-directive/templates/src/_.name.js
@@ -1,36 +1,54 @@
 
-angular.module('gm.<%= camelDirectiveName %>', [])
+(function(_) {
 
-  .constant('gm<%= firstCapCamelDirectiveName %>Config', {
-  })
+  angular.module('gm.<%= camelDirectiveName %>', [])
 
-  .controller('gm<%= firstCapCamelDirectiveName %>Controller', ['gm<%= firstCapCamelDirectiveName %>Config', function(gm<%= firstCapCamelDirectiveName %>Config) {
-  }])
-
-  .directive('gm<%= firstCapCamelDirectiveName %>', [function() {
-    return {
-      require: ['gm<%= firstCapCamelDirectiveName %>', 'ngModel'],
-      controller: 'gm<%= firstCapCamelDirectiveName %>Controller',
-      controllerAs: 'gm<%= firstCapCamelDirectiveName %>',
-      templateUrl: function(element, attrs) {
-        return attrs.templateUrl || 'gm/template/<%= directiveName %>/<%= directiveName %>.html';
-      },
-      link: function(scope, element, attrs, ctrls) {
-        var gm<%= firstCapCamelDirectiveName %>Ctrl = ctrls[0], ngModelCtrl = ctrls[1];
-
-        // model -> UI
-        ngModelCtrl.$render = function() {
-          element.html(ngModelCtrl.$modelValue);
-        };
-
-        // ui -> model
-        element.on('click', function() {
-          scope.$apply(function() {
-            ngModelCtrl.$setViewValue('clicked and modal change');
-          });
-        });
+    .constant('gm<%= firstCapCamelDirectiveName %>Config', {
+      i18nData: {
+        en: {
+          hello: 'Hello'
+        },
+        zh_CN: {
+          hello: '你好'
+        }
       }
-    }
-  }])
+    })
 
-;
+    .controller('gm<%= firstCapCamelDirectiveName %>Controller', ['gm<%= firstCapCamelDirectiveName %>Config', function(gm<%= firstCapCamelDirectiveName %>Config) {
+
+    }])
+
+    .directive('gm<%= firstCapCamelDirectiveName %>', ['gm<%= firstCapCamelDirectiveName %>Config', function(gm<%= firstCapCamelDirectiveName %>Config) {
+      return {
+        restrict: 'A',
+        require: ['gm<%= firstCapCamelDirectiveName %>', 'ngModel'],
+        controller: 'gm<%= firstCapCamelDirectiveName %>Controller',
+        controllerAs: 'gm<%= firstCapCamelDirectiveName %>',
+        templateUrl: function(element, attrs) {
+          return attrs.templateUrl || 'gm/template/<%= directiveName %>/<%= directiveName %>.html';
+        },
+        link: function(scope, element, attrs, ctrls) {
+
+          var gm<%= firstCapCamelDirectiveName %>Ctrl = ctrls[0], ngModelCtrl = ctrls[1];
+
+          scope.i18nData = gm<%= firstCapCamelDirectiveName %>Config.i18nData[attrs.gm<%= firstCapCamelDirectiveName %>Lang || 'en'];
+
+          // model -> UI
+          ngModelCtrl.$render = function() {
+            element.append(ngModelCtrl.$modelValue);
+          };
+
+          // ui -> model
+          element.on('click', function() {
+            scope.$apply(function() {
+              ngModelCtrl.$setViewValue('clicked and modal change');
+            });
+          });
+        }
+      }
+    }])
+  ;
+
+})(window._);
+
+
